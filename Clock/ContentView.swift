49a5594646b9f6034fbd81e0fcf1cc73786934ea
Larry Shannon <lawrence.t.shannon@gmail.com>
2020-09-08 14:02:36 -0700

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Larry Shannon on 9/8/20.
+//  Copyright © 2020 Larry Shannon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
